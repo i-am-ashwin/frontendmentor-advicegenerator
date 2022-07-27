308a5ab6d4e278e87d3866211de008c17052dda0
Aswin Harikumar <aswin.harikumar@yahoo.com>
2022-07-27 14:11:55 +0530

    finishing deployment

--- src/App.svelte
+++ src/App.svelte
@@ -5,8 +5,8 @@
   const mobileImg = 'assets/images/pattern-divider-mobile.svg';
   const desktopImg = 'assets/images/pattern-divider-mobile.svg';
   const btnIcon = 'assets/images/icon-dice.svg';
-  let adviceNumber = 0;
-  let advice = '';
+  let adviceNumber = 999;
+  let advice = 'Never Say Never';
   const getAdvice = async () => {
     fetch('https://api.adviceslip.com/advice')
       .then((response) => response.json())
